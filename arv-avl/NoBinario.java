3fbd21817c9ad29a8259f740c12ca2be5b271304
Luiz Felipe <darkplayertimes@gmail.com>
2025-12-03 16:26:36 +0000

    feat: no de arvore avl

--- arv-avl/NoBinario.java
+++ arv-avl/NoBinario.java
@@ -4,11 +4,13 @@ public class NoBinario<T> {
    private NoBinario<T> pai;
    private NoBinario<T> esquerda;
    private NoBinario<T> direita;
+   private int fb; // fator de balanceamento (usado em árvores AVL)
 
    // Construtor
    public NoBinario(T dado) {
        this.setDado(dado);
        // pai, esquerda e direita são 'null' por padrão
+       this.fb = 0; // fator de balanceamento inicial
    }
 
    // Retorna o dado armazenado neste nó.
@@ -54,5 +56,30 @@ public class NoBinario<T> {
       if (filho != null) {
          filho.setPai(this); // "Ligação de baixo para cima"
       }
+   }
+
+   // Retorna o fator de balanceamento.
+   public int getFb() { return this.fb; }
+
+   // Define o fator de balanceamento.
+   public void setFb(int fb) { this.fb = fb; }
+
+   // Verifica se há nó filho à esquerda.
+   public boolean temEsquerda() { return this.esquerda != null; }
+
+   // Verifica se há nó filho à direita.
+   public boolean temDireita() { return this.direita != null; }
+
+   // Verifica se há nó pai.
+   public boolean temPai() { return this.pai != null; }
+
+   // Verifica se é nó interno.
+   public boolean verificaInterno() {
+       return this.temEsquerda() || this.temDireita();
+   }
+
+   // Verifica se é nó externo (folha).
+   public boolean verificaExterno() {
+       return !this.verificaInterno();
    }
 }
